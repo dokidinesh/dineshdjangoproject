b0ab94e84b55982ce58a7fc4825de8227338eaee
dinesh <dinesh.doki1992@gmail.com>
2023-09-03 17:18:12 +0530

    changed exception raising using get method

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
 from .models import Question
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -32,11 +33,15 @@ def index(request):
 # def detail(request, question_id):
 #     return HttpResponse("You're looking at question %s." % question_id)
 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
